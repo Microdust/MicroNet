6bf9649b7a67c9206fab96b856ab139aa08ccd36
MICRODUSTPC\Microdust <thomasjj@mail.com>
2018-06-05 23:41:20 +0200

    Added a Single to utils

--- Network/NetUtilities.cs
+++ Network/NetUtilities.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MicroNet.Network
 {
+
     public static class NetUtilities
     {
         public static byte[] StringBuffer = new byte[255];
 
-
+        public static NetSingle Single = new NetSingle();
 
     }
 }
