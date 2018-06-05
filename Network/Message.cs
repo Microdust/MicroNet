d7b6269b8bbfbe34d6a1be0bf514245dc07bb1ca
MICRODUSTPC\Microdust <thomasjj@mail.com>
2018-06-05 23:42:13 +0200

    Removed packet + Single from messages

--- Network/Message.cs
+++ Network/Message.cs
@@ -7,10 +7,7 @@ using System.Threading.Tasks;
 namespace MicroNet.Network
 {
     public abstract unsafe class Message
-    {
-        internal NetSingle Single = new NetSingle();
-
-        internal ENet.Packet* Packet;
+    {       
         public DeliveryMethod DeliveryMethod;
         internal byte[] Data;
 
@@ -21,7 +18,5 @@ namespace MicroNet.Network
         {
             Data = new byte[byteSize];
         }
-
-
     }
 }
